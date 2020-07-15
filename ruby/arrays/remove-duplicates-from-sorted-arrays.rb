b424e181d6c21a67e980a4e991fd610a784a7c34
Aaron Rory Newbold <aaron.rory.newbold@gmail.com>
2020-07-15 12:55:31 -0400

    Update remove-duplicates-from-sorted-arrays.rb
    Added second solution and the runtime and memory provided by leetcode.com for each solution

--- ruby/arrays/remove-duplicates-from-sorted-arrays.rb
+++ ruby/arrays/remove-duplicates-from-sorted-arrays.rb
@@ -1,3 +1,28 @@
+# Solutions ordered by runtime speed provided by leetcode.com, fastest first, slowest last.
+
+# Solution 1
+# Runtime: 88 ms
+# Memory: 10.5 MB
+
+# @param {Integer[]} nums
+# @return {Integer}
+def remove_duplicates(nums)
+  newArray = []
+  last_num = nil
+  nums.each_with_index do |x, i|
+    newArray.push(x) if (x != last_num)
+    last_num = x
+  end
+  nums.clear
+  nums.concat(newArray)
+  puts nums.length
+end
+
+
+# Solution 2
+# Runtime: 100 ms
+# Memory: 10.8 MB
+
 # @param {Integer[]} nums
 # @return {Integer}
 def remove_duplicates(nums)
